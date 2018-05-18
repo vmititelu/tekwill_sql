d4e1bf886fd302ac1832f67262677cfdad98d4e2
vmititelu <vitalie.mititelu@gmail.com>
2018-05-18 20:31:18 +0300

    Lectia 5- primele comenzi SQL

---
+++ lesson5/Tekwill_ORADB.sql
@@ -0,0 +1,18 @@
+--SELECT * FROM EMPLOYEES
+
+--SELECT first_Name, FIRST_NAME
+--FROM employees
+
+--SELECT *
+--from EMPLOYEES
+
+--SELECT DISTINCT salary
+--from EMPLOYEES;
+
+--SELECT salary * 1.1
+--FROM employees;
+
+--SELECT first_name 
+--from EMPLOYEES
+
+DESCRIBE EMPLOYEES
